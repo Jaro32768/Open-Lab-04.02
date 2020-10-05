de2e4ec5991797ffa369f9cc76338d63b42eb886
Jaro32768 <jarko04@hotmail.sk>
2020-10-05 12:04:14 +0200

    vypracovanie

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            int length = original.Length;
+            string r = "";
+            for (int i = 0; i < length; i++)
+            {
+                r = r + original[i] + original[i];
+            }
+            return r;
         }
     }
 }
